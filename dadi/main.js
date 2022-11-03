c4f0dcef264dc810e85b8ba46bdd563c4792a6e4
KellerMarika <marikakeller92@gmail.com>
2022-11-03 22:07:03 +0100

    estetica ok

--- dadi/main.js
+++ dadi/main.js
@@ -81,17 +81,13 @@ btnGameStart_input.addEventListener("click", function () {
 
     /* iserisco il dato dell'array con la posizione === userRoll_1 */
     userScoreContainer_El.innerHTML = `
-<h2>UTENTE</h2>
-${diceIcons[userRoll_1]} + ${diceIcons[userRoll_2]}
-<h4> punteggio:${userScore}</h4>`
-
-
-
-
-
-
-
-
+    <h3>UTENTE</h3>
+    <div class="d-flex align-items-center justify-content-center py-3">
+    ${diceIcons[userRoll_1]} 
+    <i class="fs-3 px-2 fa-solid fa-plus "></i>  
+    ${diceIcons[userRoll_2]}
+    </div>
+    <h4> punteggio : ${userScore}</h4>`
     /*_______________________________________________VIENE ESEGUITO PRIMA L'ALERT E SOLO SUCCESSIVAMENTE FA INNER HTML ANCHE SE SCRITTO DOPO (ELEMENTO BLOCCANTE) funziona ma non mi piace _______________________________ */
     alert("ORA E' IL MIO TURNO!");
     // autoRoll_1= valore random 1-6
@@ -100,25 +96,28 @@ ${diceIcons[userRoll_1]} + ${diceIcons[userRoll_2]}
 
     autoScore = autoRoll_1+autoRoll_2
     console.log(autoRoll_1,"+", autoRoll_2 ,"===", autoScore );
-    alert(`HAI FATTO : ${autoRoll_1} + ${autoRoll_2} = ${autoScore} `);// _____________l'ALERT è PROPRIO BRUTTINO DA VEDERE
+    alert(`HO FATTO : ${autoRoll_1} + ${autoRoll_2} = ${autoScore} `);// _____________l'ALERT è PROPRIO BRUTTINO DA VEDERE
 
     //console.log(autoScoreContainer_El);
 
     /* iserisco il dato dell'array con la posizione === autoRoll_1 */
     autoScoreContainer_El.innerHTML = `
-<h2>UTENTE</h2>
-${diceIcons[autoRoll_1]} + ${diceIcons[autoRoll_2]}
-<h4> punteggio:${autoScore}</h4>`
+<h3>SFIDANTE</h3>
+<div class="d-flex align-items-center justify-content-center py-3">
+${diceIcons[autoRoll_1]} 
+<i class="fs-3 px-2 fa-solid fa-plus "></i>  
+${diceIcons[autoRoll_2]}
+</div>
+<h4> punteggio : ${autoScore}</h4>`
 
 
 
     if (userScore > autoScore) {
-        gameResult_El.innerText = ("HAI VINTO!!");
+        gameResult_El.innerHTML =`<h2  class="my_color-danger">HAI VINTO!!</h2>`;
     } else if (userScore === autoScore) {
-        gameResult_El.innerText = (`HANNO VINTO TUTTI!
-        (o nesuno?)`);
+        gameResult_El.innerHTML = `<h2  class="my_color-danger my-fs">HANNO VINTO TUTTI!</h2> <h2 class="text-dark my-fs">(o nesuno?)</h2>`;
     } else {
-        gameResult_El.innerText = (`HAI PERSO!`);
+        gameResult_El.innerHTML =`<h2  class="text-dark ">HAI PERSO!</h2>`;
     }
     console.log(userRoll_1, "user roll");
     /* per mettere l'immagine corrispondente dei dadi tirati a sorte, dovrei fare un array dove inserisco in ordine le icone dei dadi. occhio allo 0=1! */
@@ -130,33 +129,5 @@ ${diceIcons[autoRoll_1]} + ${diceIcons[autoRoll_2]}
 
 
 console.log("console log di elemento 3 di lista dadi", diceIcons[3])
-
-
-//button_el
-//funzione bottone
-//const userRoll_1(1)= valore random 1-6
-//_____________________________const userRoll_1(2)= valore random 1-6 se sono due
-//const user-score= userRoll_1(1)+autoroll_1(2)
-//____________________________user-score-container //innerHTML dado corrispondente
-//user-score-text_el //inner text
-
-//esce un allert l'utente fa clic
-//_________________________?? posso mettere un immagine nell'alert??? proviamo
-//const autoRoll_1= valore random 1-6
-//_____________________________const autoRoll_1(2)= valore random 1-6 se sono due
-//const auto-score= autoRoll_1(1) ______ +autoRoll_1(2)
-//____________________________auto-score-container //innerHTML dado corrispondente
-//auto-score-text_el //inner text
-
-//SE user-score > auto-score
-// game-result_El inner text:you win
-//invece SE user-score === auto-score
-// game-result_El inner text:nobady wins (or all)
-//altrimenti
-// game-result_El inner text:you have been defeated
-
-/* per mettere l'immagine corrispondente dei dadi tirati a sorte, dovrei fare un array dove inserisco in ordine le icone dei dadi. occhio allo 0=1! */
-/* poi dovrei far ciclare il dato random e quando lo trovo devo fare un innerhtml del dato dell'array */
-
 
 
