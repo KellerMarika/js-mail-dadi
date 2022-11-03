127a3be59e90b2c0b24a22e18ed4b0930237ed7e
KellerMarika <marikakeller92@gmail.com>
2022-11-03 13:48:38 +0100

    mail start

---
+++ mail/main.js
@@ -0,0 +1,17 @@
+
+/**************************************************/
+//                    E-MAIL                       //
+/**************************************************/
+
+//array di possibili indirizzi mail
+
+//trovato= false
+//prompt user-mail 
+
+//ciclo {
+//controllo se array[i] === user-mail
+//if true ---> trovato = true
+//}
+//fuori dal ciclo  
+// if trovato =true alert= true
+//if trovato =false alert= false
